99c1f6e9b941cd459a9bfcd27605da78bcf44f0e
wajima <goo.hiro.w@gmail.com>
2023-04-28 17:05:46 +0900

    props

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import TodoList from './components/TodoList';
 
 const App: React.FC = () => {
+  const todos = [{id: 't1', text: 'ミントティーを飲んだ'}];
   return (
     <div className="App">
+      <TodoList items={todos} />
     </div>
   );
 }
