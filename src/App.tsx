e5c655aa36ddf4c31abb6e041ef11bea8a989fac
wajima <goo.hiro.w@gmail.com>
2023-04-29 13:35:26 +0900

    useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import TodoList from './components/TodoList';
+import React from "react";
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
 
 const App: React.FC = () => {
-  const todos = [{id: 't1', text: 'ミントティーを飲んだ'}];
+  const todos = [{ id: "t1", text: "ミントティーを飲んだ" }];
   return (
     <div className="App">
+      <NewTodo />
       <TodoList items={todos} />
     </div>
   );
-}
+};
 
 export default App;
